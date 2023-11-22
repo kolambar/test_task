7e6c9a4084c7d78d53d322b38682be9fc94fe1cd
Nikolai <kolamba.r@gmail.com>
2023-11-22 18:01:25 +0300

    url, view and template for menu item has been made

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from menu.models import Menu
+
 
 # Create your views here.
+
+
+class MenuDetailView(DetailView):
+    model = Menu
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        current_menu = context['object']
+
+        ancestors = self.get_ancestors(current_menu)
+        context['ancestors'] = ancestors
+
+        descendants = self.get_descendants(current_menu)
+        context['descendants'] = descendants
+
+        return context
+
+    @staticmethod
+    def get_ancestors(menu):
+        ancestors = []
+
+        while menu.previous_tab:
+            ancestors.insert(0, menu.previous_tab)
+            menu = menu.previous_tab
+
+        return ancestors
+
+    @staticmethod
+    def get_descendants(menu):
+        return list(Menu.objects.filter(previous_tab=menu))
