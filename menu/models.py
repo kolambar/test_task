52b54d5e82c0f4c37e5ddb7fe8541eac87e0f4dd
Nikolai <kolamba.r@gmail.com>
2023-11-22 13:34:22 +0300

    added static, requirements made model for menu and migration. added menu to admin.py

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Вкладка меню')
+    previous_tab = models.ForeignKey('self', **NULLABLE, verbose_name='предыдущая вкладка', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
